c26a8d75beaa33e57845e84ba36a502e039a3a2f
hoapng <giahoa01giathieu@gmail.com>
2023-08-10 15:17:50 +0700

    design home page

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -5,6 +5,17 @@ const HomePage = (props) => {
             <video loop autoPlay muted >
                 <source src={videoHomePage} type="video/mp4" />
             </video>
+            <div className="homepage-content">
+                <div className='title-1'>
+                    Forms that break the norm
+                </div>
+                <div className='title-2'>
+                    Get more data—like signups, feedback, and anything else—with forms designed to be refreshingly different.
+                </div>
+                <div className='title-3'>
+                    <button>Get started—it's free</button>
+                </div>
+            </div>
         </div>
     )
 }
