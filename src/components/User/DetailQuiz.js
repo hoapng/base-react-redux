188724228c6447189dabf9b39fd5c235fb4a8723
hoapng <giahoa01giathieu@gmail.com>
2024-02-27 21:36:39 +0700

    Design Base Questions - Right Content

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,161 +1,178 @@
-import { useEffect, useState } from "react"
-import { useLocation, useParams } from "react-router-dom"
-import { getDataQuiz, postSubmitQuiz } from "../../services/apiService"
-import _ from "lodash"
-import './DetailQuiz.scss'
-import Question from "./Question"
-import ModalResult from "./ModalResult"
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { getDataQuiz, postSubmitQuiz } from "../../services/apiService";
+import _ from "lodash";
+import "./DetailQuiz.scss";
+import Question from "./Question";
+import ModalResult from "./ModalResult";
+import RightContent from "./RightContent";
 
 const DetailQuiz = (props) => {
+  const params = useParams();
 
-    const params = useParams()
+  // console.log("params", params)
 
-    // console.log("params", params)
+  const location = useLocation();
+  const quizId = params.id;
 
-    const location = useLocation();
-    const quizId = params.id
+  const [dataQuiz, setDataQuiz] = useState([]);
+  const [index, setIndex] = useState(0);
 
-    const [dataQuiz, setDataQuiz] = useState([])
-    const [index, setIndex] = useState(0)
+  const [isShowModalResult, setIsShowModalResult] = useState(false);
+  const [dataModalResult, setDataModalResult] = useState({});
 
-    const [isShowModalResult, setIsShowModalResult] = useState(false)
-    const [dataModalResult, setDataModalResult] = useState({})
+  useEffect(() => {
+    fetchQuestions();
+  }, [quizId]);
 
-    useEffect(() => {
-        fetchQuestions()
-    }, [quizId])
+  const fetchQuestions = async () => {
+    let res = await getDataQuiz(quizId);
+    // console.log('>>> check question: ', res)
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          // console.log('value', value, 'key', key)
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            // console.log('item answers', item.answers)
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            item.answers.isSelected = false;
+            answers.push(item.answers);
+          });
+          return { questionId: key, answers, questionDescription, image };
+        })
+        .value();
+      // console.log(data)
+      setDataQuiz(data);
+    }
+  };
 
-    const fetchQuestions = async () => {
-        let res = await getDataQuiz(quizId)
-        // console.log('>>> check question: ', res)
-        if (res && res.EC === 0) {
-            let raw = res.DT;
-            let data = _.chain(raw)
-                // Group the elements of Array based on `color` property
-                .groupBy("id")
-                // `key` is group's name (color), `value` is the array of objects
-                .map((value, key) => {
-                    // console.log('value', value, 'key', key)
-                    let answers = []
-                    let questionDescription, image = null;
-                    value.forEach((item, index) => {
-                        // console.log('item answers', item.answers)
-                        if (index === 0) {
-                            questionDescription = item.description;
-                            image = item.image
-                        }
-                        item.answers.isSelected = false;
-                        answers.push(item.answers)
-                    })
-                    return { questionId: key, answers, questionDescription, image }
-                })
-                .value()
-            // console.log(data)
-            setDataQuiz(data)
+  // console.log("dataQuiz", dataQuiz)
+
+  const handlePrev = () => {
+    if (index - 1 < 0) return;
+    setIndex(index - 1);
+  };
+
+  const handleNext = () => {
+    if (dataQuiz && dataQuiz.length > index + 1) setIndex(index + 1);
+  };
+
+  const handleFinishQuiz = async () => {
+    // console.log("data before submit", dataQuiz)
+    let payload = {
+      quizId: +quizId,
+      answers: [],
+    };
+    let answers = [];
+    if (dataQuiz && dataQuiz.length > 0) {
+      dataQuiz.forEach((question) => {
+        let questionId = question.questionId;
+        let userAnswerId = [];
+        question.answers.forEach((a) => {
+          if (a.isSelected === true) {
+            userAnswerId.push(a.id);
+          }
+        });
+        answers.push({
+          questionId: +questionId,
+          userAnswerId: userAnswerId,
+        });
+      });
+      payload.answers = answers;
+      // console.log("payload", payload)
+
+      let res = await postSubmitQuiz(payload);
+      // console.log('check res postSubmitQuiz', res)
+      if (res && res.EC === 0) {
+        setDataModalResult({
+          countCorrect: res.DT.countCorrect,
+          countTotal: res.DT.countTotal,
+          quizData: res.DT.quizData,
+        });
+        setIsShowModalResult(true);
+      } else {
+        alert("Sth wrong");
+      }
+    }
+  };
+
+  const handleCheckBox = (answersId, questionId) => {
+    let dataQuizClone = _.cloneDeep(dataQuiz);
+    let question = dataQuizClone.find(
+      (item) => +item.questionId === +questionId
+    );
+    if (question && question.answers) {
+      // console.log('>>> q: ', question)
+      question.answers = question.answers.map((item) => {
+        if (+item.id === +answersId) {
+          item.isSelected = !item.isSelected;
         }
+        return item;
+      });
+      // console.log(question.answers)
     }
 
-    // console.log("dataQuiz", dataQuiz)
-
-    const handlePrev = () => {
-        if (index - 1 < 0) return;
-        setIndex(index - 1)
+    let index = dataQuizClone.findIndex(
+      (item) => +item.questionId === +questionId
+    );
+    if (index > -1) {
+      dataQuizClone[index] = question;
+      setDataQuiz(dataQuizClone);
     }
+  };
 
-    const handleNext = () => {
-        if (dataQuiz && dataQuiz.length > index + 1) setIndex(index + 1)
-    }
-
-    const handleFinishQuiz = async () => {
-        // console.log("data before submit", dataQuiz)
-        let payload = {
-            quizId: +quizId,
-            answers: []
-        }
-        let answers = []
-        if (dataQuiz && dataQuiz.length > 0) {
-            dataQuiz.forEach(question => {
-                let questionId = question.questionId;
-                let userAnswerId = [];
-                question.answers.forEach(a => {
-                    if (a.isSelected === true) {
-                        userAnswerId.push(a.id)
-                    }
-                })
-                answers.push({
-                    questionId: +questionId,
-                    userAnswerId: userAnswerId
-                })
-            })
-            payload.answers = answers;
-            // console.log("payload", payload)
-
-            let res = await postSubmitQuiz(payload)
-            // console.log('check res postSubmitQuiz', res)
-            if (res && res.EC === 0) {
-                setDataModalResult({
-                    countCorrect: res.DT.countCorrect,
-                    countTotal: res.DT.countTotal,
-                    quizData: res.DT.quizData
-                })
-                setIsShowModalResult(true)
-            } else { alert('Sth wrong') }
-        }
-    }
-
-    const handleCheckBox = (answersId, questionId) => {
-        let dataQuizClone = _.cloneDeep(dataQuiz);
-        let question = dataQuizClone.find(item => +item.questionId === +questionId)
-        if (question && question.answers) {
-            // console.log('>>> q: ', question)
-            question.answers = question.answers.map(item => {
-                if (+item.id === +answersId) {
-                    item.isSelected = !item.isSelected;
-                }
-                return item
-            })
-            // console.log(question.answers)
-        }
-
-        let index = dataQuizClone.findIndex(item => +item.questionId === +questionId)
-        if (index > -1) {
-            dataQuizClone[index] = question;
-            setDataQuiz(dataQuizClone)
-        }
-    }
-
-    return (
-        <div className="detail-quiz-container">
-            <div className="left-content">
-                <div className="title">
-                    Quiz {quizId}: {location?.state?.quizTitle}
-                </div>
-                <hr />
-                <div className="q-body">
-                    <img />
-                </div>
-                <div className="q-content">
-                    <Question
-                        index={index}
-                        handleCheckBox={handleCheckBox}
-                        data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []}
-                    />
-                </div>
-                <div className="footer">
-                    <button className="btn btn-secondary" onClick={() => handlePrev()}>Prev</button>
-                    <button className="btn btn-primary" onClick={() => handleNext()}>Next</button>
-                    <button className="btn btn-warning" onClick={() => handleFinishQuiz()}>Finish</button>
-                </div>
-            </div>
-            <div className="right-content">
-                Count down
-            </div>
-            <ModalResult
-                show={isShowModalResult}
-                setShow={setIsShowModalResult}
-                dataModalResult={dataModalResult}
-            />
+  return (
+    <div className="detail-quiz-container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId}: {location?.state?.quizTitle}
         </div>
-    )
-}
-export default DetailQuiz
+        <hr />
+        <div className="q-body">
+          <img />
+        </div>
+        <div className="q-content">
+          <Question
+            index={index}
+            handleCheckBox={handleCheckBox}
+            data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []}
+          />
+        </div>
+        <div className="footer">
+          <button className="btn btn-secondary" onClick={() => handlePrev()}>
+            Prev
+          </button>
+          <button className="btn btn-primary" onClick={() => handleNext()}>
+            Next
+          </button>
+          <button
+            className="btn btn-warning"
+            onClick={() => handleFinishQuiz()}
+          >
+            Finish
+          </button>
+        </div>
+      </div>
+      <div className="right-content">
+        <RightContent dataQuiz={dataQuiz} />
+      </div>
+
+      <ModalResult
+        show={isShowModalResult}
+        setShow={setIsShowModalResult}
+        dataModalResult={dataModalResult}
+      />
+    </div>
+  );
+};
+export default DetailQuiz;
--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,52 +1,66 @@
-import _ from "lodash"
+import _ from "lodash";
+import { useState } from "react";
+import Lightbox from "react-awesome-lightbox";
 
 const Question = (props) => {
+  const { data, index, handleCheckBox } = props;
 
-    const { data, index, handleCheckBox } = props
+  const [isPreviewImage, setIsPreviewImage] = useState(false);
 
-    if (_.isEmpty(data)) return (<></>)
+  if (_.isEmpty(data)) return <></>;
 
-    const handleHandleCheckBox = (event, aId, qId) => {
-        // console.log("check: ", event.target.checked)
-        // console.log("data props: ", data)
-        // console.log("aId: ", aId, "qId: ", qId)
-        handleCheckBox(aId, qId)
-    }
+  const handleHandleCheckBox = (event, aId, qId) => {
+    // console.log("check: ", event.target.checked)
+    // console.log("data props: ", data)
+    // console.log("aId: ", aId, "qId: ", qId)
+    handleCheckBox(aId, qId);
+  };
 
-    return (
-        <>
-            {data.image ?
-                <div className="q-image">
-                    <img src={`data:image/png;base64,${data.image}`} />
+  return (
+    <>
+      {data.image ? (
+        <div className="q-image">
+          <img
+            style={{ cursor: "pointer" }}
+            onClick={() => setIsPreviewImage(true)}
+            src={`data:image/png;base64,${data.image}`}
+          />
+          {isPreviewImage === true && (
+            <Lightbox
+              image={`data:image/png;base64,${data.image}`}
+              title={"Question Image"}
+              onClose={() => setIsPreviewImage(false)}
+            ></Lightbox>
+          )}
+        </div>
+      ) : (
+        <div className="q-image"></div>
+      )}
+      <div className="question">
+        Question {index + 1}: {data.questionDescription}
+      </div>
+      <div className="answer">
+        {data.answers &&
+          data.answers.length &&
+          data.answers.map((a, index) => {
+            return (
+              <div key={`answers-${index}`} className="a-child">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    checked={a.isSelected}
+                    onChange={(event) =>
+                      handleHandleCheckBox(event, a.id, data.questionId)
+                    }
+                  />
+                  <label className="form-check-label">{a.description}</label>
                 </div>
-                :
-                <div className="q-image">
-
-                </div>
-            }
-            <div className="question">
-                Question {index + 1}: {data.questionDescription}
-            </div>
-            <div className="answer">
-                {data.answers && data.answers.length && data.answers.map((a, index) => {
-                    return (
-                        <div key={`answers-${index}`} className="a-child">
-                            <div className="form-check">
-                                <input
-                                    className="form-check-input"
-                                    type="checkbox"
-                                    checked={a.isSelected}
-                                    onChange={(event) => handleHandleCheckBox(event, a.id, data.questionId)}
-                                />
-                                <label className="form-check-label">
-                                    {a.description}
-                                </label>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-        </>
-    )
-}
-export default Question
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+export default Question;
