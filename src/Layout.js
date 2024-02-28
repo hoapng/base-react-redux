99b4e33098e54810fc90423b0b147361bf2fc5ab
hoapng <giahoa01giathieu@gmail.com>
2024-02-28 16:51:18 +0700

    Design DashBoard

--- src/Layout.js
+++ src/Layout.js
@@ -2,7 +2,7 @@ import App from "./App";
 import User from "./components/User/User";
 import Admin from "./components/Admin/Admin";
 import HomePage from "./components/Home/HomePage";
-import Dashboard from "./components/Admin/Content/Dashboard";
+import DashBoard from "./components/Admin/Content/DashBoard";
 import ManageUser from "./components/Admin/Content/ManageUser";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
@@ -47,7 +47,7 @@ const Layout = (props) => {
               </PrivteRoute>
             }
           >
-            <Route index element={<Dashboard />} />
+            <Route index element={<DashBoard />} />
             <Route path="manage-users" element={<ManageUser />} />
             <Route path="manage-quizzes" element={<ManageQuiz />} />
             <Route path="manage-questions" element={<Question />} />
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,31 +1,42 @@
 import SideBar from "./SideBar";
-import './Admin.scss';
-import { FaBars } from 'react-icons/fa';
+import "./Admin.scss";
+import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import PerfectScrollbar from 'react-perfect-scrollbar'
+import PerfectScrollbar from "react-perfect-scrollbar";
+import Language from "../Header/Language";
+import { NavDropdown } from "react-bootstrap";
 // import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = (props) => {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
 
-    return (
-        <div className="admin-container">
-            <div className="admin-sidebar">
-                <SideBar collapsed={collapsed} />
-            </div>
-            <div className="admin-content">
-                <div className="admin-header">
-                    <FaBars onClick={() => setCollapsed(!collapsed)} />
-                </div>
-                <div className="admin-main">
-                    <PerfectScrollbar>
-                        <Outlet />
-                    </PerfectScrollbar>
-                </div>
-            </div>
-            {/* <ToastContainer
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBar collapsed={collapsed} />
+      </div>
+      <div className="admin-content">
+        <div className="admin-header">
+          <span onClick={() => setCollapsed(!collapsed)}>
+            <FaBars className="leftside" />
+          </span>
+          <div className="rightside">
+            <Language />
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log out</NavDropdown.Item>
+            </NavDropdown>
+          </div>
+        </div>
+        <div className="admin-main">
+          <PerfectScrollbar>
+            <Outlet />
+          </PerfectScrollbar>
+        </div>
+      </div>
+      {/* <ToastContainer
                 position="top-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -39,7 +50,7 @@ const Admin = (props) => {
             />
             Same as
             <ToastContainer /> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 export default Admin;
